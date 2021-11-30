88cc07a7b5873413d435b29bc35ed75122f5da57
Yonghyeon-Choi <choiyh0601@naver.com>
2021-11-30 23:05:05 +0900

    1130

--- downloader.py
+++ downloader.py
@@ -1,17 +1,15 @@
 import pandas as pd
 import os
 
-#예시파일입니당. 경로랑 파일은 넣어주세용
-df = pd.read_csv(os.path.join('video', 'keyboard.csv'))
+df = pd.read_csv(os.path.join('csv', 'keyboard.csv'))
 
-#pip install pytube
+# pip install pytube
 from pytube import YouTube
 
-#저장 파일 경로만 그때그때 바꿔주세용!
 file_path = os.path.join('video', 'keyboard')
 for i in range(len(df)):
     print(i, df.iloc[i]['url'])
     yt = YouTube(df.iloc[i]['url'])
     yt_streams = yt.streams
-    my_stream = yt_streams.get_by_itag("18")
+    my_stream = yt_streams.get_by_itag("18") # 18 means 320p
     my_stream.download(output_path=file_path)
